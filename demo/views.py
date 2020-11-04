9b9a323bc0e0c413c560e71985e33c9bfcd3556f
jdiminic <jdiminic@gmail.com>
2020-11-04 20:28:50 +0100

    save all

--- demo/views.py
+++ demo/views.py
@@ -1,10 +1,15 @@
 from django.http import HttpResponse
 
-
 from django.shortcuts import render
 
 
 # Create your views here.
 
 def index(req):
-    return HttpResponse("dela ovo")
+    print("ovo je log neki")
+    return render(req, "demo/home.html")
+
+
+def template(req):
+    data = {"name": "janko diminic"}
+    return render(req, "demo/index.html", data)
